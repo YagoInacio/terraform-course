9df9ff6069e584085324418c8e76534ed28f2e63
Yago Inacio <yagofaran@gmail.com>
2022-09-10 21:26:22 +0000

    connected subnet to the internet

--- main.tf
+++ main.tf
@@ -19,3 +19,42 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+/*
+Create a route table (connected with an igw) and explicitly associate it with a subnet
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "assoc-rtb-subnet" {
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+*/
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" { # using the auto created (default) route table, connecting it to an igw
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
