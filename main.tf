3ec74ab68cd8915b4c55088657ee9f7e9711e65b
Yago Inacio <yagofaran@gmail.com>
2022-09-18 18:30:09 +0000

    creating an ec2 instance

--- main.tf
+++ main.tf
@@ -119,3 +119,45 @@ resource "aws_default_security_group" "myapp-default-sg" {  # Configuring the de
     Name = "${var.env_prefix}-default-sg"
   }
 }
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server_key"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.myapp-default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
--- variables.tf
+++ variables.tf
@@ -14,3 +14,6 @@ variable "env_prefix" {}
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
+
+variable "instance_type" {}
+variable "public_key_location" {}
