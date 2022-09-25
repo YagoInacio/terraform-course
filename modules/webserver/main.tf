f2afeeb44ebb86e278e976cc757c3472449220e4
Yago Inacio <yagofaran@gmail.com>
2022-09-25 19:32:41 +0000

    replaced developed subnet module with existing vpc one from registry

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "myapp-server" {
   instance_type = var.instance_type
 
   subnet_id = var.subnet_id
-  vpc_security_group_ids = [aws_default_security_group.myapp-default-sg.id]
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
